db3df43357c90638cdc6377786d0e2a9970c867b
tedoang <jokov.unknown@gmail.com>
2022-02-07 17:23:25 +0200

    added delete function

--- ShoppingList/App.js
+++ ShoppingList/App.js
@@ -1,11 +1,33 @@
-import React from 'react';
-import {View, Text, StyleSheet} from 'react-native';
+import React, {useState} from 'react';
+import {View, Text, StyleSheet, FlatList} from 'react-native';
 import Header from './components/Header.js';
+import ListItem from './components/ListItem.js';
+import uuid from 'uuidv4';
 
 const App = () => {
+  const [items, setItems] = useState([
+    {id: uuid(), text: 'Milk'},
+    {id: uuid(), text: 'Eggs'},
+    {id: uuid(), text: 'Beef'},
+    {id: uuid(), text: 'Cola'},
+    {id: uuid(), text: 'voda'},
+    {id: uuid(), text: 'hlqb'},
+    {id: uuid(), text: 'maslo'},
+  ]);
+  const deleteItem = id => {
+    setItems(prevItems => {
+      return prevItems.filter(item => item.id != id);
+    });
+  };
   return (
     <View style={styles.constainer}>
       <Header title="Shopping list" />
+      <FlatList
+        data={items}
+        renderItem={({item}) => (
+          <ListItem item={item.text} itemId={item.id} deleteItem={deleteItem} />
+        )}
+      />
     </View>
   );
 };
